556a91f5809fae7d83289883f536d1459d126d50
Apach <pavel_andreev@inbox.ru>
2023-08-08 16:53:57 +0400

    StatsService

---
+++ src/main/java/ru/netology/stats/Application.java
@@ -0,0 +1,13 @@
+package ru.netology.stats;
+
+import ru.netology.stats.StatService.test.StatsService;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Application {
+    public static void main(String[] args) {
+        StatsService service = new StatsService();
+
+
+    }
+}
